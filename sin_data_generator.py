8ff16cafe51d74da197198469aa4698d9e751cae
Fuad Kehinde <141517152+FuadK01@users.noreply.github.com>
2024-02-21 14:23:00 +0000

    Update sin_data_generator.py

--- sin_data_generator.py
+++ sin_data_generator.py
@@ -46,8 +46,7 @@ def sin_gen(snr, freq, length):
             labels.append(label)
             # Randomised Frequency of the sine wave
             frequency = np.random.randint(freq[0], freq[1])
-            phase = np.random.randint(0, data_length) # Randomised phase
-            signal = np.sin((frequency * x) + phase)
+            signal = np.sin((frequency * x))
             noise = np.random.normal(0, snr, data_length)
             output = signal + noise
 
@@ -61,7 +60,7 @@ def sin_gen(snr, freq, length):
 
     return dataset
 
-def multi_sin_gen(snr, freq_1, freq_2, length):
+def multi_sin_gen(snr, freq_1, freq_2, freq_3, length):
     # Number of data points in each graph
     data_length = 256
 
@@ -70,7 +69,7 @@ def multi_sin_gen(snr, freq_1, freq_2, length):
 
     for i in range(0, length):
         # Randomly decides if the data will be noise (0), signal with freq_1 (1), signal with freq_2 (2)
-        label = np.random.randint(0,3)
+        label = np.random.randint(0,4)
 
         if label == 0:
             x = np.linspace(0,0,data_length)
@@ -83,8 +82,7 @@ def multi_sin_gen(snr, freq_1, freq_2, length):
             labels.append(label)
             # Randomised Frequency of the sine wave
             frequency = np.random.randint(freq_1[0], freq_1[1])
-            phase = np.random.randint(0, data_length) # Randomised phase
-            signal = np.sin((frequency * x) + phase)
+            signal = np.sin((frequency * x))
             noise = np.random.normal(0, snr, data_length)
             output = signal + noise
         
@@ -93,8 +91,16 @@ def multi_sin_gen(snr, freq_1, freq_2, length):
             labels.append(label)
             # Randomised Frequency of the sine wave
             frequency = np.random.randint(freq_2[0], freq_2[1])
-            phase = np.random.randint(0, data_length) # Randomised phase
-            signal = np.sin((frequency * x) + phase)
+            signal = np.sin((frequency * x))
+            noise = np.random.normal(0, snr, data_length)
+            output = signal + noise
+        
+        elif label == 3:
+            x = np.linspace(0, 2*np.pi, data_length)
+            labels.append(label)
+            # Randomised Frequency of the sine wave
+            frequency = np.random.randint(freq_3[0], freq_3[1])
+            signal = np.sin((frequency * x))
             noise = np.random.normal(0, snr, data_length)
             output = signal + noise
 
@@ -107,7 +113,6 @@ def multi_sin_gen(snr, freq_1, freq_2, length):
     dataset = [outputs, labels]
 
     return dataset
-
 
 def multi_plot_gen(snr, freq_1, freq_2, freq_3, length):
     # Number of data points in each graph
@@ -171,8 +176,66 @@ def multi_plot_gen(snr, freq_1, freq_2, freq_3, length):
 
     return dataset
 
+def multi_time_plot_gen(snr, freq_1, freq_2, freq_3, length):
+    # Number of data points in each graph
+    data_length = 256
+
+    outputs = []
+    labels = []
+
+    for i in range(0, length):
+        # Randomly decides if the data will be noise (0), sinusoidal signal with freq_1 (1), combined sinusoidal signal with freq_2 (2),
+        # and exponential signal with freq_3 (3)
+        label = np.random.randint(0,4)
+
+        if label == 0:
+            x = np.linspace(0,0,data_length)
+            labels.append(label)
+            noise = np.random.normal(0, snr, data_length) # Generates random gaussian noise with sigma=snr
+            output = x + noise
+        
+        elif label == 1:
+            x = np.linspace(0, 2*np.pi, data_length)
+            labels.append(label)
+            # Randomised Frequency of the sine wave
+            frequency_1 = np.random.randint(freq_1[0], freq_1[1])
+            frequency_2 = np.random.randint(freq_1[2], freq_1[3])
+            signal = np.cos(frequency_1 * x + frequency_2 * x ** 2)
+            noise = np.random.normal(0, snr, data_length)
+            output = signal + noise
+        
+        elif label == 2:
+            x = np.linspace(0, 2*np.pi, data_length)
+            labels.append(label)
+            # Randomised Frequency of the sine wave
+            frequency_1 = np.random.randint(freq_2[0], freq_2[1])
+            frequency_2 = np.random.randint(freq_2[2], freq_2[3])
+            signal = np.cos(frequency_1 * x + frequency_2 * x ** 2)
+            noise = np.random.normal(0, snr, data_length)
+            output = signal + noise
+
+        elif label == 3:
+            x = np.linspace(0, 2*np.pi, data_length)
+            labels.append(label)
+            # Randomised Frequency of the sine wave
+            frequency_1 = np.random.randint(freq_3[0], freq_3[1])
+            frequency_2 = np.random.randint(freq_3[2], freq_3[3])
+            signal = np.cos(frequency_1 * x + frequency_2 * x ** 2)
+            noise = np.random.normal(0, snr, data_length)
+            output = signal + noise
+
+        else:
+            raise Exception("Error encountered whilst producing sin plots. Should be impossible, check random label generator")
+        
+        output /= np.sqrt(np.sum(np.abs(output)**2))
+        outputs.append(output)
+
+    dataset = [outputs, labels]
+
+    return dataset
 
 if __name__ == "__main__":
     sin_gen(0.5, [10,30], 10)
     multi_sin_gen(0.5, [10, 30], [60, 80], 10)
     multi_plot_gen(0.5, [10, 30], [60, 80], [100, 120], 10)
+    multi_time_plot_gen(0.5, [10, 30], [60, 80], [100, 120], 10)
